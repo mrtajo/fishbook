340d9072e7d6dd25058f0d1b214c49adee2a6bca
mrtajo <mrtajo@gmail.com>
2015-06-15 23:38:30 +0900

    Initial Commit

---
+++ fishbook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fishbook
+//
+//  Created by mrtajo on 2015. 6. 15..
+//  Copyright (c) 2015년 mrtajo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fishbook/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fishbook
+//
+//  Created by mrtajo on 2015. 6. 15..
+//  Copyright (c) 2015년 mrtajo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
